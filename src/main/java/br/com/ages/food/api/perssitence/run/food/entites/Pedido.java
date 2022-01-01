14d2e6edcd287f851cec308bb834d2c886dcfc97
Ismael Nascimento Sousa <ismaelturbo55@gmail.com>
2022-01-01 15:25:37 -0300

    Realizado a associação da classe Pedido com Cliente.
    Realizado o mapeamento da associação Unidirecional M para 1, da classe Pedido com a classe cliente, a classe Pedido é a classe dona do relacioanemto.

--- src/main/java/br/com/ages/food/api/perssitence/run/food/entites/Pedido.java
+++ src/main/java/br/com/ages/food/api/perssitence/run/food/entites/Pedido.java
@@ -9,7 +9,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
 
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
@@ -46,6 +50,11 @@ public class Pedido implements Serializable {
 	@Column(name ="ped_versao")
 	private Integer versao;
 	private static final long serialVersionUID = 1L;
+	
+	//Mapeamento dos relacionamentos
+	@ManyToOne
+	@JoinColumn(name = "ped_cliente_cli_id")
+	Cliente cliente;
 	
 	public Pedido(LocalDateTime dataCriacao, LocalDateTime dataUtlimaAtualizacao, LocalDateTime dataConclusao,
 			BigDecimal total, Integer versao) {
