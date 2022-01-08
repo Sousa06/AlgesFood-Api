446d6f82cd53173decfd031a04df38c3520ab2d5
Ismael Nascimento Sousa <ismaelturbo55@gmail.com>
2022-01-07 21:40:13 -0300

    Realizado o mapeamento do relacionamento da entidade ItemPedido com ItemPedidoPK.

--- src/main/java/br/com/ages/food/api/perssitence/run/food/entites/ItemPedido.java
+++ src/main/java/br/com/ages/food/api/perssitence/run/food/entites/ItemPedido.java
@@ -23,13 +23,9 @@ import lombok.Setter;
 @Table(name = "item_pedido",schema = "algesfood")
 @Entity
 public class ItemPedido implements Serializable {
-
-	@Setter(value = AccessLevel.NONE)
-	@EqualsAndHashCode.Include
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ite_id")
-	private Long id;
+    
+	@EmbeddedId
+    private ItemPedidoPK id;
 	@Column(name = "ite_preco_produto")
 	private BigDecimal precoProduto;
 	@Column(name = "ite_quantidade")
@@ -37,6 +33,16 @@ public class ItemPedido implements Serializable {
 	@Column(name = "ite_versao")
 	private Integer versao;
 	private static final long serialVersionUID = 1L;
+	
+	// Atributos do relacionamento.
+	@MapsId("pedidoId")
+	@ManyToOne
+	@JoinColumn(name = "pedido_id")
+	private Pedido pedido;
+	@MapsId("produtoId")
+	@ManyToOne
+	@JoinColumn(name="produto_id")
+	private Produto produto;
 
 	public ItemPedido(BigDecimal precoProduto, Integer quantidade, Integer versao) {
 		
